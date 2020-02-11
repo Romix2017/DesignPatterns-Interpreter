19728d187da2269c26a6137c5b6af8fa98cbf8df
Roman Karpov <poma.05@mail.ru>
2020-02-11 17:13:13 +0400

    init

---
+++ Interpreter/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+
+namespace Interpreter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Context context = new Context();
+            ArrayList list = new ArrayList();
+            list.Add(new TerminalExpression());
+            list.Add(new NonterminalExpression());
+            list.Add(new TerminalExpression());
+            list.Add(new TerminalExpression());
+            foreach (AbstractExpression exp in list)
+            {
+                exp.Interpret(context);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ InterpreterRealWorld/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InterpreterRealWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string roman = "MCMXXVIII";
+            Context context = new Context(roman);
+            List<Expression> tree = new List<Expression>();
+            tree.Add(new ThousandExpression());
+            tree.Add(new HundredExpression());
+            tree.Add(new TenExpression());
+            tree.Add(new OneExpression());
+            foreach (Expression exp in tree)
+            {
+                exp.Interpet(context);
+            }
+            Console.WriteLine("{0} = {1}", roman, context.Output);
+            Console.ReadKey();
+        }
+    }
+}
